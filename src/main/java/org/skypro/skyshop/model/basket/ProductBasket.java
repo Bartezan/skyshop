a8d834d474f384fee6982fb885d3c92c082c7ab9
Bartezan <Bartez2202@gmail.com>
2025-09-11 17:06:14 +1000

    hw2-v2

--- src/main/java/org/skypro/skyshop/model/basket/ProductBasket.java
+++ src/main/java/org/skypro/skyshop/model/basket/ProductBasket.java
@@ -1,10 +1,15 @@
 package org.skypro.skyshop.model.basket;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+@Component
+@SessionScope
 public class ProductBasket {
 
     private final Map<UUID, Integer> basketMap;
